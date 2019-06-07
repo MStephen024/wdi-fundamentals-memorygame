2aba73daa33c3856e0603e746574748a76a2e19a
Stephen Melendez <Space-Time@Stephens-MBP.fios-router.home>
2019-06-07 03:33:15 -0400

    created game logic, post-objects update

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -15,7 +15,28 @@ console.log("User flipped " + cardOne);
 cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);*/
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -29,7 +50,9 @@ function checkForMatch() {
 flipCard(0);
 flipCard(2);
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId]);
 	checkForMatch();
 	if (cardsInPlay.length === 2) {
