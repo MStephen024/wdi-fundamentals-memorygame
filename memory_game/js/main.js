a276894166683a58d646cdc930ace47fb5732afb
Stephen Melendez <Space-Time@Stephens-MBP.fios-router.home>
2019-06-07 03:16:32 -0400

    created game logic, post-functions update

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -7,21 +7,36 @@ var cardFour = "king";
 console.log("User flipped" + cardOne);
 console.log("User flipped" + cardFour);*/
 
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-var cardOne = cards[0];
+/*var cardOne = cards[0];
 var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
+	cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
 
 cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardTwo);*/
 
-if (cardsInPlay.length === 2) {
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+	} else {
+  console.log("Sorry, try again.");
+	}
+}
+
+flipCard(0);
+flipCard(2);
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
+}
 }
